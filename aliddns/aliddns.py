8f583e80c77c08581f3009cd79bf9e7c4bb258e9
Ziyang Li <zylan29@outlook.com>
2022-10-24 15:50:50 +0800

    Support multiple resource records

--- aliddns/aliddns.py
+++ aliddns/aliddns.py
@@ -14,7 +14,7 @@ ipv6_api = 'api-ipv6.ip.sb'
 ipv4_api_url = 'https://' + ipv4_api + '/ip'
 ipv6_api_url = 'https://' + ipv6_api + '/ip'
 
-default_RR = '@'
+default_RRs = ['@', '*']
 
 ipv4_record_type = 'A'
 ipv6_record_type = 'AAAA'
@@ -97,45 +97,53 @@ class AliDDNS(object):
 
         return localIPv4, localIPv6
     
-    def _ddns(self, domainName: str, recordType: str, publicIP: str):
+    def _ddns(self, domainName: str, recordType: str, resourceRecords: list, publicIP: str):
         domain = self.describe(domainName, recordType)
         if domain['TotalCount'] == 0:
-            self.add(default_RR, domainName, recordType, publicIP)
-            print('{} Add {} record for {}.{}: {}'.format(datetime.now(), recordType, default_RR, domainName, publicIP))
+            for resourceRecord in resourceRecords:
+                self.add(resourceRecord, domainName, recordType, publicIP)
+                print('{} Add {} record for {}.{}: {}'.format(datetime.now(), recordType, resourceRecord, domainName, publicIP))
         elif domain['TotalCount'] > 0:
-            if domain['TotalCount'] > 1:
-                print('{} CAUTION: Found {} {} records for {}. Simply update the first one!'.format(datetime.now(), domain['TotalCount'], recordType, domainName))
-
-            record = domain['DomainRecords']['Record'][0]
-            recordIPAddr = record['Value'].strip()
-            if recordIPAddr != publicIP:
-                self.update(record['RR'], record['RecordId'], recordType, publicIP)
-                print('{} Update {} record for {}.{}: from {} to {}'.format(datetime.now(), recordType, record['RR'], domainName, recordIPAddr, publicIP))
+            records = domain['DomainRecords']['Record']
+            for resourceRecord in resourceRecords:
+                found = False
+                for record in records:
+                    if record['RR'] == resourceRecord and record['Type'] == recordType:
+                        found = True
+                        recordIPAddr = record['Value'].strip()
+                        if recordIPAddr != publicIP:
+                            self.update(record['RR'], record['RecordId'], recordType, publicIP)
+                            print('{} Update {} record for {}.{}: from {} to {}'.format(datetime.now(), recordType, record['RR'], domainName, recordIPAddr, publicIP))
+                    if not found:
+                        self.add(resourceRecord, domainName, recordType, publicIP)
+                        print('{} Add {} record for {}.{}: {}'.format(datetime.now(), recordType, resourceRecord, domainName, publicIP))
     
-    def ddns(self, domainName: str):
+    def ddns(self, domainName: str, resourceRecords=default_RRs):
         localIPv4, localIPv6 = self.get_local_ip()
         publicIPv4, publicIPv6 = self.get_publib_ip()
 
-        if publicIPv4 != '' and localIPv4 == publicIPv4:
-            self._ddns(domainName, ipv4_record_type, publicIPv4)
-
-        if publicIPv6 != '' and localIPv6 == publicIPv6:
-            self._ddns(domainName, ipv6_record_type, publicIPv6)
-
         if publicIPv4 == '' and publicIPv6 == '':
             print('WARN: no public IP found.')
+            return
+
+        if publicIPv4 != '' and localIPv4 == publicIPv4:
+            self._ddns(domainName, ipv4_record_type, resourceRecords, publicIPv4)
+
+        if publicIPv6 != '' and localIPv6 == publicIPv6:
+            self._ddns(domainName, ipv6_record_type, resourceRecords, publicIPv6)
     
 
 def main():
     parser = argparse.ArgumentParser(prog='aliddns', description='Ali DDNS command-line tool.')
     parser.add_argument('-k', '--accesskey-id', help='AccessKey ID')
     parser.add_argument('-s', '--accesskey-secret', help='AccessKey secret')
-    parser.add_argument('-r', '--region-id', default='cn-hangzhou', help='Region ID')
     parser.add_argument('-d', '--domain-name', help='Your domain name')
+    parser.add_argument('-r', '--resource-records', type=list, default=default_RRs, help='Your resource record, e.g. @ *')
+    parser.add_argument('--region-id', default='cn-hangzhou', help='Region ID')
 
     args = parser.parse_args()
     
     client = AcsClient(args.accesskey_id, args.accesskey_secret, args.region_id)
     ddns = AliDDNS(client)
-    ddns.ddns(args.domain_name)
+    ddns.ddns(args.domain_name, args.resource_records)
     
